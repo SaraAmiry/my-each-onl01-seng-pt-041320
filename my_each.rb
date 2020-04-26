b9745c43b31fe8c31cce28f3b6f2bc7edddfe50e
Sara Amiry <saraamiry24@gmail.com>
2020-04-26 10:10:15 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,9 @@
-def my_each # put argument(s) here
-  # code here
+
+def my_each(collection) # put argument(s) here
+    puts i 
+    #yield
 end
+collection = [1, 2, 3, 4]
+my_each(collection) do |i|
+  puts i 
+end 
