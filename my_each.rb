3e46c7835f61cf9e48f77d71b30c35b90b147c48
Sara Amiry <saraamiry24@gmail.com>
2020-04-26 10:14:13 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,9 +1,8 @@
-
-def my_each(collection) # put argument(s) here
-    puts i 
-    #yield
-end
 collection = [1, 2, 3, 4]
+def my_each(collection) # put argument(s) here
+   collection.forEach(element => console.log(element));
+end
+
 my_each(collection) do |i|
   puts i 
 end 
